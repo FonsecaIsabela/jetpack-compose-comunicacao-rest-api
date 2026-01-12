bcee3f0b7f0d31ff515d98bba7500c77a9509ea5
Isabela Fonseca <isabela.fonseca@fulltime.com.br>
2026-01-12 16:05:20 -0300

    Anyflix
    Utilizando REST API em apps Android
    Conectando o App na REST API com o Retrofit
    Integrando a REST API
    Integrando todas as telas com a REST API

---
+++ app/src/main/java/br/com/alura/anyflix/di/modules/RestApiModule.kt
@@ -0,0 +1,52 @@
+package br.com.alura.anyflix.di.modules
+
+import br.com.alura.anyflix.network.services.MovieService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RestApiModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("http://192.168.1.104:8080/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieService(retrofit: Retrofit): MovieService {
+        return retrofit.create(MovieService::class.java)
+    }
+
+    // Logging Inteceptor: por meio de logs apreseent mais detalhes da requisição,
+    // como por exemplo, o endereco, verbo, headers, respostas...
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            setLevel(HttpLoggingInterceptor.Level.BODY)
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+}
---
+++ app/src/main/java/br/com/alura/anyflix/ui/viewmodels/HomeViewModel.kt
@@ -0,0 +1,62 @@
+package br.com.alura.anyflix.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.alura.anyflix.ui.uistates.HomeUiState
+import br.com.alura.anyflix.ui.viewmodels.repositories.MovieRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val repository: MovieRepository,
+) : ViewModel() {
+
+    private var currentUiStateJob: Job? = null
+    private val _uiState = MutableStateFlow<HomeUiState>(
+        HomeUiState.Loading
+    )
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        loadUiState()
+    }
+
+    private fun loadUiState() {
+        currentUiStateJob?.cancel()
+        currentUiStateJob = viewModelScope.launch {
+            repository.findSections().onStart {
+                _uiState.update { HomeUiState.Loading }
+            }.collectLatest { sections ->
+                if (sections.isEmpty()) {
+                    delay(3000)
+                    _uiState.update {
+                        HomeUiState.Empty
+                    }
+                } else {
+                    val movie = sections
+                        .entries.random()
+                        .value.random()
+                    _uiState.update {
+                        HomeUiState.Success(
+                            sections = sections,
+                            mainBannerMovie = movie
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    fun loadSections() {
+        loadUiState()
+    }
+}
